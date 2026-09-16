b6b68dc49d36653fb48a26eaa863d38fe30f3237
Oskar Sharipov <oskargit@riseup.net>
2026-09-16 14:54:11 +0700

    Specify endpoint prefix

--- service/api/__main__.py
+++ service/api/__main__.py
@@ -6,7 +6,7 @@ from service.api.conf import settings
 from service.api.endpoints import router
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(router, prefix="/api/v1")
 
 
 if __name__ == "__main__":
