ce43aff79b42aecaf9ec1dc790b4f302d14e04cb
Oskar Sharipov <oskargit@riseup.net>
2026-09-17 14:58:18 +0700

    Add logging

---
+++ src/logging_config.py
@@ -0,0 +1,28 @@
+import logging
+import logging.config
+
+
+def setup_logging() -> None:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "standard",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "root": {
+            "level": "INFO",
+            "handlers": ["console"],
+        },
+    }
+
+    logging.config.dictConfig(LOGGING)
